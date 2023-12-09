1f34b9f6c5157aacb7289bf7fa03147227d8cd31
Saeid Keyvanfar <kayvanfar.sj@gmail.com>
2023-12-09 13:23:06 +0100

    add Conditional

--- src/main/kotlin/ir/sk/kolinbasic/Conditional.kt
+++ src/main/kotlin/ir/sk/kolinbasic/Conditional.kt
@@ -1,2 +1,68 @@
 package ir.sk.kolinbasic
 
+fun main() {
+    println(maxOf(2, 6))
+
+    // foreach
+    val items = listOf("first", "second", "third")
+    for(item in items) {
+        println(item)
+    }
+
+    // for
+    for (index in items.indices) {
+        println("item at $index is ${items[index]}")
+    }
+
+    // while
+    var index = 0
+    while (index < items.size) {
+        println("item at $index is ${items[index]}")
+        index++
+    }
+
+
+    // range
+    val x = 10
+    val y = 9
+    if (x in 1..y+1) {
+        println("fits in range")
+    }
+
+    // not in range
+    val list = listOf("a", "b", "c")
+
+    if (-1 !in 0..list.lastIndex) {
+        println("-1 is out of range")
+    }
+    if (list.size !in list.indices) {
+        println("list size is out of valid list indices range, too")
+    }
+
+    // iterate over a range
+    for (x in 1..5) {
+        print(x)
+    }
+
+    // Progression
+    for (x in 1..10 step 2) {
+        print(x)
+    }
+    println()
+    for (x in 9 downTo 0 step 3) {
+        print(x)
+    }
+}
+
+// if
+fun maxOf(a: Int, b: Int) = if (a > b) a else b
+
+fun descripe(obj: Any): String =
+    when (obj) {
+        1 -> "One"
+        "Hello" -> "Greating"
+        is Long -> "Long"
+        !is String -> "Not a String"
+        else -> "Unknown"
+    }
+
