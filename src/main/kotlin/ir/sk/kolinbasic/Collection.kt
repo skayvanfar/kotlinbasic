d4e0cd06dc22c212118b2c0f31bb6eb1d6ef0928
Saeid Keyvanfar <kayvanfar.sj@gmail.com>
2023-12-09 13:41:04 +0100

    update Collection

--- src/main/kotlin/ir/sk/kolinbasic/Collection.kt
+++ src/main/kotlin/ir/sk/kolinbasic/Collection.kt
@@ -1,11 +1,22 @@
 package ir.sk.kolinbasic
 
 fun main() {
+    // list
     val items = listOf("first", "second", "third")
+
+    // map
+    val map = mapOf("a" to 1, "b" to 2, "c" to 3)
+    println(map["key"])
+    val mutableMap = mutableMapOf("a" to 1, "b" to 2, "c" to 3)
+    mutableMap["a"] = 3
 
     // iteration
     for (item in items) {
         println(item)
+    }
+
+    for ((k, v) in map) {
+        println("$k -> $v")
     }
 
     // stream
@@ -15,10 +26,11 @@ fun main() {
         .map { it.uppercase() }
         .forEach { println(it) }
 
-    //automatic casting
+    // automatic casting
     val a : Any = 10;
     if (a is String) {
-        // `obj` is automatically cast to `String` in this branch
+        // obj` is automatically cast to `String` in this branch
         println(a.length)
     }
+
 }
