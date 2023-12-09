c1fe40b7de893bf23670d3bfae54e077f870a609
Saeid Keyvanfar <kayvanfar.sj@gmail.com>
2023-12-09 13:40:32 +0100

    add Extention

---
+++ src/main/kotlin/ir/sk/kolinbasic/Extention.kt
@@ -0,0 +1,10 @@
+package ir.sk.kolinbasic
+
+fun main() {
+    // Extension functions
+    fun String.spaceToCamelCase() {
+        // do somthing
+    }
+
+    "Convert this to camelcase".spaceToCamelCase()
+}
