707ce1136409ce7d8cbf64e83f7d7ab10a43e52c
Saeid Keyvanfar <kayvanfar.sj@gmail.com>
2023-12-09 14:24:35 +0100

    update Clazz

--- src/main/kotlin/ir/sk/kolinbasic/Clazz.kt
+++ src/main/kotlin/ir/sk/kolinbasic/Clazz.kt
@@ -1,8 +1,19 @@
 package ir.sk.kolinbasic
 
+import java.math.BigDecimal
+
 fun main() {
     val rectangle = Rectangle(2.4, 5.4)
     println("The perimeter  is ${rectangle.perimeter}")
+
+    // Configure properties of an object (apply), configuring properties that aren't present in the object constructor.
+    val myRectangle = Rectangle(2.1, 4.5).apply {
+        //
+    }
+
+    // todo
+    fun calcTaxes(): BigDecimal = TODO("Waiting for feedback from accounting")
+
 }
 
 class Rectangle(val height: Double, val length: Double) {
