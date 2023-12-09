a76059e0374d55703c47c0df9e9afcb1e3548e02
Saeid Keyvanfar <kayvanfar.sj@gmail.com>
2023-12-09 13:45:17 +0100

    add abstraction to Inheritance

--- src/main/kotlin/ir/sk/kolinbasic/Inheritance.kt
+++ src/main/kotlin/ir/sk/kolinbasic/Inheritance.kt
@@ -1,7 +1,27 @@
 package ir.sk.kolinbasic
 
+fun main() {
+    val myObject = object : MyAbstractClass() {
+        override fun doSomething() {
+            // ...
+        }
+
+        override fun sleep() { // ...
+        }
+    }
+    myObject.doSomething()
+}
+
 open class Shape
 
+// inheritance
 class NewRectangle(val height: Double, val length: Double): Shape() {
     val perimeter = (height + length) * 2
 }
+
+// abstraction
+abstract class MyAbstractClass {
+    abstract fun doSomething()
+    abstract fun sleep()
+}
+
