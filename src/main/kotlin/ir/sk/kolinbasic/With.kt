1af7d1fb031f004ed64d42d180dba0baeb7df48c
Saeid Keyvanfar <kayvanfar.sj@gmail.com>
2023-12-09 14:20:18 +0100

    add With

---
+++ src/main/kotlin/ir/sk/kolinbasic/With.kt
@@ -0,0 +1,20 @@
+package ir.sk.kolinbasic
+
+fun main() {
+    val myTurtle = Turtle()
+    with(myTurtle) { //draw a 100 pix square
+        penDown()
+        for (i in 1..4) {
+            forward(100.0)
+            turn(90.0)
+        }
+        penUp()
+    }
+}
+
+class Turtle {
+    fun penDown() {}
+    fun penUp() {}
+    fun turn(degrees: Double) {}
+    fun forward(pixels: Double) {}
+}
